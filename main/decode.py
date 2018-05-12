56bc0a98d5ab8e3c21c5c728734c4feb0c1cc7ae
mcding <mcding@mit.edu>
2018-05-12 00:16:44 -0400

    finial revision and tuning before submission

--- main/decode.py
+++ main/decode.py
@@ -5,17 +5,17 @@ from solver import solve
 def decode(ciphertext, output_file_name): 
     lp = load_prob('./data/ngrams')
     wlp = load_word_prob('./data/word')
-    num = 10
+    num = 20
     order = 28
     maxiter = 5000
     verbose_interval = 0
-    w = [1, 0.3, 16]
-    threshold = 0.8
+    w = [1, 0.3, 10]
+    threshold = 0.9
     use_mask = True
     fix_interval = 200
     testcc = to_code(ciphertext)
     testdc, f_pred = solve(testcc, lp, wlp, num, order, maxiter, verbose_interval, 
-                           w, threshold, use_mask, fix_interval)
+                           w, threshold, fix_interval)
     decipheredtext = ''.join(to_text(testdc))
     f = open(output_file_name, 'w')
     f.write(decipheredtext)
--- main/solver.py
+++ main/solver.py
@@ -90,19 +90,21 @@ def ngram_log_prob(x, lp, c, w):
     return x_lp
 
 def word_log_prob(x, wlp, ciphercode, w):
+    length = ciphercode.shape[0]
+    if length < 400:
+        return np.zeros(x.shape[0])
     xinv = np.argsort(x, axis=1)
-    testciphercode = ciphercode[:768]
-    decryptedcode = xinv[:,testciphercode]
+    decryptedcode = xinv[:,ciphercode[:400]]
     x_lp = np.zeros(x.shape[0])
     for i in range(x.shape[0]):
         wl = decryptedcode[i,]
         wl = wl[wl!=27]
         wl = ''.join(to_text(wl))
         wl = wl.split(' ')
-        if len(wl)<64:
-            x_lp[i] = 64*(-40)
+        if len(wl) < 40:
+            x_lp[i] = 40*(-40)
             continue
-        for wd in wl[:64]:
+        for wd in wl[:40]:
             x_lp[i] += wlp.get(wd, -40)
     return x_lp*w[2]
 
@@ -140,49 +142,28 @@ def grammar_validity(x, ciphercode):
     plaincode = xinv[:,ciphercode]
     return 1-np.mean(np.any(np.logical_and(plaincode[:,:-1] == 27, plaincode[:,1:] != 26), axis=1))
 
-def show_errormap(errormap):
-    plt.figure(figsize=(16,14))
-    sns.heatmap(error_map)
-    plt.show()
-    
-def solve(testcc, lp, wlp, num, order, maxiter, verbose_interval, w, threshold, use_mask, fix_interval, testpc=None, f_true=None):
+def solve(testcc, lp, wlp, num, order, maxiter, verbose_interval, w, threshold, fix_interval):
     c = count(testcc, order)
     x = initialize(lp, c, num, order)
     rlp = reshape_lp(lp, num)
     rs_cum = np.zeros((num,1))
     xmj_cum = np.zeros((1,order))
     m, xmj = mask(x, threshold)
-    it = 0
     for it in range(maxiter):
         m, xmjp = mask(x, threshold)
-        if use_mask:
-            m, xmj_cum = cumulate(xmj, xmjp, xmj_cum, m, fix_interval)
+        m, xmj_cum = cumulate(xmj, xmjp, xmj_cum, m, fix_interval)
         xmj = xmjp
-        if np.mean(m) == 0:
+        if np.mean(m) == 0 and it > 3*fix_interval:
             break
-        if use_mask:
-            x, xp = change_with_mask(x, m, xmj)
-        else:
-            x, xp = change_without_mask(x, m, xmj)
+        x, xp = change_with_mask(x, m, xmj)
         p_x = ngram_log_prob(x, rlp, c, w) + word_log_prob(x, wlp, testcc, w)
-        xb = best(x, p_x)
         p_xp = ngram_log_prob(xp, rlp, c, w) + word_log_prob(x, wlp, testcc, w)
         p_delta = p_xp - p_x
         x, rs = update(x, xp, p_delta)
         rs_cum += rs
-        if testpc is None and f_true is None:
-            acc = map_acc = gm_v = mj_macc = b_macc = np.nan
-        else:     
-            acc = accuracy(x, testcc, testpc)
-            map_acc = mapping_accuracy(x, f_true)
-            gm_v = grammar_validity(x, testcc)
-            mj_macc = mapping_accuracy(xmj, f_true)
-            b_macc = mapping_accuracy(xb, f_true)
-            er_map = error_map(xmj, f_true)
         if verbose_interval > 0 and it % verbose_interval == 0:
-            print("it:{}, log_p:{:1.3e}, acpt_r:{:1.3e}, acc:{:1.3e}, macc:{:1.3e}, p_fix:{:1.3e}, b_macc:{:1.3e}".format(
-                  str(it).zfill(4), np.mean(p_x), np.mean(rs_cum)/verbose_interval, acc, map_acc, 1-np.mean(m), b_macc))
+            print("it:{}, log_p:{:1.3e}, acpt_r:{:1.3e}, p_fix:{:1.3e}".format(
+                  str(it).zfill(4), np.mean(p_x), np.mean(rs_cum)/verbose_interval, 1-np.mean(m)))
             rs_cum = np.zeros((num,1))
     xmj = xmj.reshape(xmj.shape[-1])
-    xb = xb.reshape(xb.shape[-1])
-    return np.argsort(xb)[testcc], xb
+    return np.argsort(xmj)[testcc], xmj
